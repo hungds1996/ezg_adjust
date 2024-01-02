051d264932a3c072f5af2d11a0a365cf65e9e571
HungDS <hungds1996@gmail.com>
2024-01-02 11:20:05 +0700

    remove credentials

--- config.py
+++ config.py
@@ -1,38 +1,61 @@
 import datetime
 from google.cloud import bigquery
 
-
+# default end date is yesterday
 end_date = str(datetime.date.today() - datetime.timedelta(days=1))
-json_keys = 'data-import-409408-97adc8924579.json'
 
-api_token = 'yJLV5vBb2-LhGpc4Tukj'
-apps_token = ['9qr22ik5f30g','weh64bmgqg3k']
-kpis = ['install_cost', 'click_cost', 'impression_cost','cost','ad_impressions','ad_revenue','paid_installs','paid_clicks','paid_impressions','clicks', 'impressions', 'installs']
-groupby='apps,days,networks,countries'
+# file name of service account json key
+json_keys = "data-import-409408-97adc8924579.json"
 
+# adjust account api token
+api_token = "<adjust API token>"
+
+# app token in adjust dashboard
+apps_token = ["9qr22ik5f30g", "weh64bmgqg3k"]
+
+# Metrics and dimensions
+kpis = [
+    "install_cost",
+    "click_cost",
+    "impression_cost",
+    "cost",
+    "ad_impressions",
+    "ad_revenue",
+    "paid_installs",
+    "paid_clicks",
+    "paid_impressions",
+    "clicks",
+    "impressions",
+    "installs",
+]
+groupby = "apps,days,networks,countries"
+
+# local temp file name
 temp_file = ".\\data\\adjuts_data.csv "
 
+# game info include app name, table id (bigquery table) , app token, app id
 app_info = [
     {
         "app_name": "Hero Survival",
         "info": {
-                    "table_id": "data-import-409408.temp_tables.hero_survival_adjust",
-                    "app_token": "9qr22ik5f30g",
-                    "app_id": "hero_survival"
-                }
+            "table_id": "data-import-409408.temp_tables.hero_survival_adjust",
+            "app_token": "9qr22ik5f30g",
+            "app_id": "hero_survival",
+        },
     },
     {
         "app_name": "Nightfall",
         "info": {
-                    "table_id": "data-import-409408.temp_tables.nightfall_adjust",
-                    "app_token": "weh64bmgqg3k",
-                    "app_id": "nightfall"
-                }
-    }
+            "table_id": "data-import-409408.temp_tables.nightfall_adjust",
+            "app_token": "weh64bmgqg3k",
+            "app_id": "nightfall",
+        },
+    },
 ]
 
+# email account to send mail
 email_config = {
-    "sender": "hungds1996@gmail.com",
-    "password": "pvbm vfld coeq wohw",
+    "sender": "<email address>",
+    "password": "<email password or app password>",
     "receiver": ["hungds1996@gmail.com"],
 }
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from aggregated_data_importer import *
 from config import *
 
-if __name__ == '__main__':
-    try: 
-        start_date, end_date = get_start_end_date('adjust_aggregrated_data_daily')
+if __name__ == "__main__":
+    try:
+        start_date, end_date = get_start_end_date("adjust_aggregrated_data_daily")
         df = pull_adjust_data(start_date, end_date)
         total_rows = import_data_bigquery(df)
-        import_logger(df['date'].max(), 'adjust_aggregrated_data_daily', total_rows)
+        import_logger(df["date"].max(), "adjust_aggregrated_data_daily", total_rows)
     except Exception as e:
         logger.info(e, exc_info=True)
 
-        # send_email(
-        #     email_config["receiver"],
-        #     e,
-        # )
-
-
-        
-        
+        send_email(
+            email_config["receiver"],
+            e,
+        )
